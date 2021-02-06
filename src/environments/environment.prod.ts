667ce3a3d24412ce702ee8d02892cf2121b5e3a0
MonkeyScript <monkeyscript.dev@gmail.com>
2021-02-06 20:37:25 +0530

    Add gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYTkY4UBV2CJBggAvxL4HSoE4tocxJ1xk",
+    authDomain: "gist-lite.firebaseapp.com",
+    projectId: "gist-lite",
+    storageBucket: "gist-lite.appspot.com",
+    messagingSenderId: "969811026502",
+    appId: "1:969811026502:web:0579fd9b12ebcae8ede047",
+    measurementId: "G-5SN9JC9ZHM"
+  }
 };
